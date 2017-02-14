ea099748e31573469deb749e8a65f8ff4c45738a
Unknwon <u@gogs.io>
2017-02-14 15:37:48 -0500

    Add support for custom hooks

--- hook.go
+++ hook.go
@@ -14,12 +14,16 @@ import (
 	"github.com/Unknwon/com"
 )
 
-// hookNames is a list of Git server hooks' name that are supported.
-var hookNames = []string{
-	"pre-receive",
-	// "update",
-	"post-receive",
-}
+var (
+	// Direcotry of hook file. Can be changed to "custom_hooks" for very purpose.
+	HookDir = "hooks"
+	// HookNames is a list of Git server hooks' name that are supported.
+	HookNames = []string{
+		"pre-receive",
+		"update",
+		"post-receive",
+	}
+)
 
 var (
 	ErrNotValidHook = errors.New("not a valid Git hook")
@@ -27,7 +31,7 @@ var (
 
 // IsValidHookName returns true if given name is a valid Git hook.
 func IsValidHookName(name string) bool {
-	for _, hn := range hookNames {
+	for _, hn := range HookNames {
 		if hn == name {
 			return true
 		}
@@ -51,7 +55,7 @@ func GetHook(repoPath, name string) (*Hook, error) {
 	}
 	h := &Hook{
 		name: name,
-		path: path.Join(repoPath, "hooks", name),
+		path: path.Join(repoPath, HookDir, name),
 	}
 	if isFile(h.path) {
 		data, err := ioutil.ReadFile(h.path)
@@ -74,7 +78,7 @@ func (h *Hook) Name() string {
 	return h.name
 }
 
-// Update updates hook settings.
+// Update updates content hook file.
 func (h *Hook) Update() error {
 	if len(strings.TrimSpace(h.Content)) == 0 {
 		if isExist(h.path) {
@@ -91,8 +95,8 @@ func ListHooks(repoPath string) (_ []*Hook, err error) {
 		return nil, errors.New("hooks path does not exist")
 	}
 
-	hooks := make([]*Hook, len(hookNames))
-	for i, name := range hookNames {
+	hooks := make([]*Hook, len(HookNames))
+	for i, name := range HookNames {
 		hooks[i], err = GetHook(repoPath, name)
 		if err != nil {
 			return nil, err
