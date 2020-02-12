7acaeaec59382b3f5371ba2462eaf2ed6f5b670f
ᴜɴᴋɴᴡᴏɴ <u@gogs.io>
2020-02-12 13:34:31 +0800

    repo_pull: add unit tests

---
+++ repo_pull_test.go
@@ -0,0 +1,43 @@
+package git
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRepository_MergeBase(t *testing.T) {
+	t.Run("no merge base", func(t *testing.T) {
+		mb, err := testrepo.MergeBase("0eedd79eba4394bbef888c804e899731644367fe", "bad_revision")
+		assert.Equal(t, ErrNoMergeBase, err)
+		assert.Empty(t, mb)
+	})
+
+	tests := []struct {
+		base         string
+		head         string
+		opt          MergeBaseOptions
+		expMergeBase string
+	}{
+		{
+			base:         "4e59b72440188e7c2578299fc28ea425fbe9aece",
+			head:         "0eedd79eba4394bbef888c804e899731644367fe",
+			expMergeBase: "4e59b72440188e7c2578299fc28ea425fbe9aece",
+		},
+		{
+			base:         "master",
+			head:         "release-1.0",
+			expMergeBase: "0eedd79eba4394bbef888c804e899731644367fe",
+		},
+	}
+	for _, test := range tests {
+		t.Run("", func(t *testing.T) {
+			mb, err := testrepo.MergeBase(test.base, test.head, test.opt)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			assert.Equal(t, test.expMergeBase, mb)
+		})
+	}
+}
