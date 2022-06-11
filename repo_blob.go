53aa96b7a80fd22ed05b98f297a998e88ae9c056
jonatan <jonatan5524@gmail.com>
2022-06-11 14:39:12 +0300

    feat(repo_blob): add CatFileBlob

---
+++ repo_blob.go
@@ -0,0 +1,28 @@
+// Copyright 2015 The Gogs Authors. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package git
+
+// CatFileBlob returns the blob corresponding to the given revision of the repository.
+func (repo *Repository) CatFileBlob(rev string) (*Blob, error) {
+	typ, err := repo.CatFileType(rev)
+	if err != nil {
+		return nil, err
+	}
+
+	if typ != ObjectBlob {
+		return nil, ErrNotBlob
+	}
+
+	return &Blob{
+		TreeEntry: &TreeEntry{
+			mode: EntryBlob,
+			typ:  ObjectBlob,
+			id:   MustIDFromString(rev),
+			parent: &Tree{
+				repo: repo,
+			},
+		},
+	}, nil
+}
