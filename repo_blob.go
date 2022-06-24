03c927cf5d7e47e1aad1d8107e6d0fc5fc30ab88
jonatan <jonatan5524@gmail.com>
2022-06-24 13:07:42 +0300

    fix compiler errors

--- repo_blob.go
+++ repo_blob.go
@@ -4,8 +4,26 @@
 
 package git
 
+import "time"
+
+// CatFileBlobOptions contains optional arguments for verifying the objects.
+//
+// Docs: https://git-scm.com/docs/git-cat-file#Documentation/git-cat-file.txt
+type CatFileBlobOptions struct {
+	// The timeout duration before giving up for each shell command execution.
+	// The default timeout duration will be used when not supplied.
+	Timeout time.Duration
+	// The additional options to be passed to the underlying git.
+	CommandOptions
+}
+
 // CatFileBlob returns the blob corresponding to the given revision of the repository.
-func (r *Repository) CatFileBlob(rev string) (*Blob, error) {
+func (r *Repository) CatFileBlob(rev string, opts ...CatFileBlobOptions) (*Blob, error) {
+	var opt CatFileBlobOptions
+	if len(opts) > 0 {
+		opt = opts[0]
+	}
+
 	rev, err := r.RevParse(rev, RevParseOptions{Timeout: opt.Timeout}) //nolint
 	if err != nil {
 		return nil, err
